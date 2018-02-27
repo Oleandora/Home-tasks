2577462f8245fdccf74dcb8b1a9bb36e1db6c507
Olga_S <Oleandora@gmail.com>
2018-02-28 00:38:07 +0300

    output reformatted

--- time.py
+++ time.py
@@ -7,9 +7,9 @@ date_now = datetime.now()
 date_yesterday = date_now - timedelta(days=1)
 date_month_ago = date_now - timedelta(days=30)
 
-print('Время сейчас: {0} \n' \
-	  'Вчера: {1} \n' \
-	  'Месяц назад: {2} \n'.format(date_now, date_yesterday, date_month_ago))
+print('''Время сейчас: {0} \n
+        Вчера: {1} \n
+        Месяц назад: {2} \n'''.format(date_now, date_yesterday, date_month_ago))
 
 date_string = "01/01/17 12:10:03.234567"
 date_formatted = datetime.strptime(date_string, '%d/%m/%y %H:%M:%S.%f')
