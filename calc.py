b640b46afd8b73bdd440a97f5c21ff1ddce19ecb
Olga_S <Oleandora@gmail.com>
2018-02-27 23:39:10 +0300

    variables renamed, exceptions handling redesigned, main added

--- calc.py
+++ calc.py
@@ -1,31 +1,31 @@
-oper_tuple = ('+','-', '/', '*')
-evaluation = input(': ')
-
-def calc(a, b, oper):
+def calculate(operand1, operand2, sign):
     try:
-        a, b = float(a), float(b)
-    except ValueError:
-        print('Некорректный ввод, нет одного или двух чисел')    
-    try:
-        op = {'+': (a+b), '-': (a-b),
-              '*': (a*b), '/': (a/b)}
-        result = op[oper]
+        operand1, operand2 = float(operand1), float(operand2)
+        calculation = {'+': lambda x, y: x + y,
+                       '-': lambda x, y: x - y,
+                       '*': lambda x, y: x * y,
+                       '/': lambda x, y: x / y}
+        result = calculation[sign](operand1, operand2)
         return result
-    except ZeroDivisionError:
-        print('Ай-яй-яй! Деление на ноль!')
-    
+    except ValueError as e:
+        return 'Некорректный ввод, нет одного или двух чисел: {}'.format(e.args[0])
+    except ZeroDivisionError as e:
+        return 'Ай-яй-яй! Деление на ноль! Фу быть таким!: {}'.format(e.args[0])
 
-if evaluation.endswith('='):
-    evaluation = evaluation.strip().replace('=','')
-    f = 1
-    for operation in oper_tuple:
-        if evaluation.find(operation) != -1:
-            a, b = evaluation.split(operation)
-            print(calc(a, b, operation))
-            f = 0
-            break    
-    if f:
-        print('Нет математического оператора')
-    
-else:
-    print('Некорректный ввод, нет знака "="')
+
+if __name__ == '__main__':
+
+    sign_tuple = ('+', '-', '/', '*')
+    evaluation = input('Введите математическое выражение со знаком "=" в конце: ')
+
+    if evaluation.endswith('='):
+        evaluation = evaluation.strip().replace('=','')
+        for operation in sign_tuple:
+            if operation in evaluation:
+                operand1, operand2 = evaluation.split(operation)
+                print(calculate(operand1, operand2, operation))
+                break
+        else:
+            print('Нет математического оператора')
+    else:
+        print('Некорректный ввод, нет знака "="')
