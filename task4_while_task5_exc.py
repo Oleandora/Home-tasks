380aadce555f52b11462b27fadf1e76f54b54aec
Olga_S <Oleandora@gmail.com>
2018-03-06 02:17:33 +0300

    tabs replaced with 4 spaces

--- task4_while_task5_exc.py
+++ task4_while_task5_exc.py
@@ -1,54 +1,54 @@
 persons = ["Вася", "Маша", "Петя", "Валера", "Саша", "Даша"]
 
+
 def find_person(name):
-	for i, person in enumerate(persons):
-		while person in name:
-			print('{0} нашёлся!'.format(persons.pop(i)))
-			break
-	print('{0} не найден'.format(name))		
+    for i, person in enumerate(persons):
+        while person in name:
+            print('{0} нашёлся!'.format(persons.pop(i)))
+            break
+    print('{0} не найден'.format(name))
+
 
 def ask_user():
+    print('\nПривет!')
+    while True:
+        try:
+            answer = input('Как дела? ')
+            answer = answer.lower()
+            if answer == 'хорошо':
+                break
+        except KeyboardInterrupt:
+            print('\nПока!')
+            break
 
-	print('\nПривет!')
-	while True:
-		try:
-			answer = input('Как дела? ')
-			answer = answer.lower()
-			if answer == 'хорошо':
-				break
-		except KeyboardInterrupt:
-			print('\nПока!')
-			break
+    get_answer()
 
-	get_answer()
 
 def get_answer():
+    words_to_answer = {"привет": "И тебе привет!",
+                       "как дела?": "Лучше всех.", "пока": "Увидимся!"}
 
-	words_to_answer = {"привет": "И тебе привет!", 
-	"как дела?": "Лучше всех.", "пока": "Увидимся!"}
+    while True:
+        try:
+            question = input('Напиши что-нибудь: ')
 
-	while True:
-		try:
-			question = input('Напиши что-нибудь: ')
-		
-			if isinstance(question, int) or isinstance(question, float):
-				print("Вы ввели число, я не смогу ответить")
-			else:
-				answer = words_to_answer.get(question)
+            if isinstance(question, int) or isinstance(question, float):
+                print("Вы ввели число, я не смогу ответить")
+            else:
+                answer = words_to_answer.get(question)
 
-				if answer == 'None':
-					print('Не знаю, что на это ответить.')	
-				elif question == 'пока':
-					print(answer)
-					break
-				print(answer)
+                if answer == 'None':
+                    print('Не знаю, что на это ответить.')
+                elif question == 'пока':
+                    print(answer)
+                    break
+                print(answer)
 
-		except KeyboardInterrupt:
-			print('\nПока!')
-			break
+        except KeyboardInterrupt:
+            print('\nПока!')
+            break
+
 
 a = input('Введите имя: ')
 find_person(a)
 ask_user()
-
-
--- wordcount.py
+++ wordcount.py
@@ -1,30 +1,28 @@
-stroka = input('Напишите в кавычках слова, которые нужно посчитать')
-special_char = ('@','#','$','%','^','&','*','-','+','/','=','!','?')
+input_data = input('Напишите в кавычках слова, которые нужно посчитать')
+special_char = ('@', '#', '$', '%', '^', '&', '*', '-', '+', '/', '=', '!', '?')
 
-if stroka:
-	if stroka.startswith('"') and stroka.endswith('"'):
-		words = stroka.replace('"','')
-		words = words.strip()
-		words = words.split()
-		print(words)
-		number_of_words = len(words)
-		for word in words:
-			if word in special_char:
-				number_of_words -= 1
-			try:
-				if float(word):
-					number_of_words -= 1
-				elif int(word):
-					number_of_words -= 1
-			except ValueError:
-				pass
-	else:
-		print('Вы не написали слова в кавычках')
-		number_of_words = 0			
+if input_data:
+    if input_data.startswith('"') and input_data.endswith('"'):
+        words = input_data.replace('"', '')
+        words = words.strip()
+        words = words.split()
+        print(words)
+        number_of_words = len(words)
+        for word in words:
+            if word in special_char:
+                number_of_words -= 1
+            try:
+                if float(word):
+                    number_of_words -= 1
+                elif int(word):
+                    number_of_words -= 1
+            except ValueError:  # if it can't convert to number, it's char
+                pass
+    else:
+        print('Вы не написали слова в кавычках')
+        number_of_words = 0
 else:
-	print('Вы ничего не написали')
-	number_of_words = 0
-
+    print('Вы ничего не написали')
+    number_of_words = 0
 
 print('Количество слов: ', number_of_words)
-
