4ba5d31680b50b86c5d430216460b4128a890dcb
Olga_S <Oleandora@gmail.com>
2018-03-20 23:32:17 +0300

    Refactoring, key removed

--- baby_name.py
+++ baby_name.py
@@ -1,35 +1,48 @@
-from datetime import datetime
-from flask import Flask, abort, request
-import requests, pprint
+from flask import Flask, request
+import requests
+import os
 
-key = '5a8160579c036312bb4163fd1fbf9767'
+key = os.environ.get('DATA_MOS_API_KEY')
 app = Flask(__name__)
 
-@app.route("/")
-def index():
-    return "Hello, adventurer!"
 
-@app.route("/names")
-def names():
+@app.route('/')
+def index():
+    return 'Hello, adventurer!'
+
+
+@app.route('/names')
+def names_by_year():
     year = int(request.args.get('year'))
-    data = get_data_from_url("http://api.data.mos.ru/v1/datasets/2009/rows?api_key={0}".format(key))
-    body = "Данные за {0} год.\n<table border=1><tr><th>Имя</th><th>Кол-во пиплов</th><th>Месяц</th></tr><tr>".format(year)
+    data = get_data()
+    body = '''Данные за {0} год.
+    <table border=1>
+        <tr>
+            <th>Имя</th>
+            <th>Кол-во пиплов</th>
+            <th>Месяц</th>
+        </tr>
+        <tr>'''.format(year)
     for row in data:
         if row['Cells']['Year'] == year:
-            body += "<td>{0}</td><td>{1}</td><td>{2}</td></tr><tr>".format(row['Cells']['Name'],
-                                                                           row['Cells']['NumberOfPersons'],
-                                                                           row['Cells']['Month'])
-
-    body += "</tr></table>"
+            body += '''<td>{0}</td>
+                       <td>{1}</td>
+                       <td>{2}</td>
+                       </tr> 
+                       <tr>'''.format(row['Cells']['Name'],
+                                      row['Cells']['NumberOfPersons'],
+                                      row['Cells']['Month'],
+                                      )
+    body += '</tr></table>'
     return body
 
 
-def get_data_from_url(url):
-    result = requests.get(url)
+def get_data():
+    result = requests.get('http://api.data.mos.ru/v1/datasets/2009/rows', params={'api_key': key})
     if result.status_code == 200:
         return result.json()
     else:
-        print("Error")
+        print('Error')
 
 
 if __name__ == '__main__':
